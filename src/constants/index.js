a3028fd430b167e794a459673f737b9e424a43e4
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-15 17:49:25 +0530

    app codes

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `Full-Stack development student based in Pune, India. I am an Information Technology graduate from RTMNU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using MERN Stack. Working for myself to improve my skills.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Engineer Intern",
+    company: "Scalefull Technologies LLP.",
+    description: `Created RESTful APIs using Node.js and Express.js to handle CRUD operations, authentication, and authorization. Carried out simple front-end tasks with JavaScript, HTML, CSS, and Bootstrap.`,
+    technologies: ["Javascript", "React.js", "Express.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "System Engineer",
+    company: "Atos Global IT Solutions and Services",
+    description: ` Contributed to the Support background team, where my primary responsibility was debugging Windows OS issues. Provided help to end customers in several areas with technical support, fixing problems with hardware, software, and network connectivity. Managed backup and recovery procedures, making sure vital information was consistently backed up and easily restored in case of system failure or data loss.`,
+    technologies: ["Windows Administrator", "SNOW", "Troubleshooting"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Summer Intern",
+    company: "Cojag Smart Technology Pvt. Ltd. ",
+    description: `Helped with the creation and upkeep of the business's HTML, CSS, and JavaScript website. Used responsive design methods to help improve user interface (UI) and user experience (UX). Helped collect and prepare data from multiple sources, such as database searches and web scraping. Data was cleaned and arranged to guarantee consistency and accuracy. Utilized Python and Pandas to analyze datasets and generate actionable insights. Created visualizations using Matplotlib and Seaborn to present findings to the team.`,
+    technologies: ["Python", "HTML", "CSS", "Jupyter Notebook"],
+  },
+  {
+    year: "2021 - 2021",
+    role: "Summer Intern",
+    company: "Bennett University",
+    description: `During this internship, I worked on AI/ML projects and learned the basics of Python. This internship allowed me to apply theoretical knowledge in a practical setting, develop key technical skills, and enhance my problem-solving and teamwork abilities in a dynamic and collaborative environment.`,
+    technologies: ["Python", "Machine Learning", "AI"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Pune, Maharashtra, India.",
+  phoneNo: "+91 - 8551929956",
+  email: "shriyasorte2000@gmail.com",
+};
