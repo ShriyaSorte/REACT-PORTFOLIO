9af6dd85d1e91b985b64b0028918d85298dac6f6
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-20 23:05:11 +0530

    changes in logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,20 @@ import logo from "../assets/ss_logo.png";
 const Navbar = () => {
   return (
     <nav className="mb-3 flex items-center justify-between">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="h-[140px] w-[140px]" src={logo} alt="" />
+      <div className="flex flex-shrink-0 items-center ml-2">
+        <img
+          className="h-[100px] w-[100px] mt-3"
+          src={logo}
+          alt="logo"
+        />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/shriya-sorte/"><FaLinkedin/></a>
-        <a href="https://github.com/ShriyaSorte"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/shriya-sorte/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/ShriyaSorte">
+          <FaGithub />
+        </a>
       </div>
     </nav>
   );
