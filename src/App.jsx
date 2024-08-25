d4f0768f1af276dae48f691499b11a51c8f94970
Shriya Sorte <shriyasorte2000@gmail.com>
2024-08-25 21:46:42 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
-
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
